f2e2f0fc2384f7a5f564900bbfd1499d19efe1b4
Barbosa <renan.a.barbosa@capgemini.com>
2024-07-18 09:19:09 -0300

    terceiro commit

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -10,10 +10,10 @@ describe('challenge test switch', () => {
   it.only('Cadastro com dados válidos',() =>{
     cy.get('input[id="signup-personal-data-firstName"]').type('Renan')
     cy.get('input[id="signup-personal-data-lastName"]').type('Ramos Barbosa')
-    cy.get('input[id="signup-personal-data-birthDate"]').type('24112000')
-    cy.get('input[id="signup-personal-data-cpf"]').type('02361015013')
-    cy.get('input[id="signup-personal-data-email"]').type('lorena5065@uorak.com')
-    cy.get('input[id="signup-personal-data-email-confirm"]').type('lorena5065@uorak.com')
+    cy.get('input[id="signup-personal-data-birthDate"]').type('01012000')
+    cy.get('input[id="signup-personal-data-cpf"]').type('62685367039')
+    cy.get('input[id="signup-personal-data-email"]').type('lorena5066@uorak.com')
+    cy.get('input[id="signup-personal-data-email-confirm"]').type('lorena5066@uorak.com')
     cy.get('input[id="signup-personal-data-password"]').type('Renanteste2024')
     cy.get('input[id="signup-personal-data-password-confirm"]').type('Renanteste2024')
     cy.get('button').contains('Selecione a proficiência...').click();
@@ -26,11 +26,11 @@ describe('challenge test switch', () => {
     cy.get('button[id="signup_submit_button_3"]').click()
     cy.get('button[id="wide_window_button"]').should('be.visible')
     cy.get('button[id="wide_window_button"]').click()
-    cy.get('input[id="user_login"]').type('lorena5065@uorak.com')
+    cy.get('input[id="user_login"]').type('lorena5066@uorak.com')
     cy.get('input[id="user_pass"]').type('Renanteste2024')
     cy.get('input[id="wp-submit"]').click()
     cy.location('pathname').should('equal','/dashboard/')
-
+    
   })
 
 
